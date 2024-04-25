1f4debc98e43b18431d53967efc375e097b8d3a1
prashantsingh9079 <prashant.singh9079@gmail.com>
2024-04-25 15:49:37 +0530

    simple display on app (msg)

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
 const http = require('http');
+const fs = require('fs');
+const data = [];
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
     // console.log("req");
     const url = req.url;
-    if(url=='/home'){
-        res.setHeader("Content-Type","text/html")
+    const method = req.method;
+    if (url == '/home') {
+        res.setHeader("Content-Type", "text/html")
         res.write('<h1>home</h1>');
         return res.end()
     }
-    else if(url=='/admin'){
-        res.setHeader("Content-Type","text/html")
+    else if (url == '/form') {
+        res.setHeader("Content-Type", "text/html").write(`<html><p>${data}</p><form action="/show" method="POST"> <input type="text" name="entered"><button type="submit">Click</button></form></html>`);
+        res.end()
+        
+    }
+    else if (url == '/show' && method == 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody.split("=")[1]);
+            data.push(parsedBody.split("=")[1]);
+            fs.writeFile("abc.txt", parsedBody, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+                res.statusCode=302;
+                res.setHeader("Location", "/form")
+                res.end();
+                // res.write('<h1>see logs</h1>');
+            })
+        })
+
+    }
+    else if (url == '/admin') {
+        res.setHeader("Content-Type", "text/html")
         res.write('<h1>admin</h1>');
         return res.end()
     }
 })
 
-server.listen(3000);
+server.listen(5000);
