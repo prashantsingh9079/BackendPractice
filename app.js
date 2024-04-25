96b58291acf69f9d2b64a67935dcf69a33045f74
prashantsingh9079 <prashant.singh9079@gmail.com>
2024-04-25 09:58:25 +0530

    sending response

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log("req");
+    // console.log("req");
+    const url = req.url;
+    if(url=='/home'){
+        res.setHeader("Content-Type","text/html")
+        res.write('<h1>home</h1>');
+        return res.end()
+    }
+    else if(url=='/admin'){
+        res.setHeader("Content-Type","text/html")
+        res.write('<h1>admin</h1>');
+        return res.end()
+    }
 })
 
 server.listen(3000);
